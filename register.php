2d4dd72968e870112b65cc7f761379cbb0711e13
YashJoshi26 <132135133+YashJoshi26@users.noreply.github.com>
2023-05-02 14:06:25 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,119 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = '';
+}
+;
+
+if (isset($_POST['submit'])) {
+
+    $name = $_POST['name'];
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $email = $_POST['email'];
+    $email = filter_var($email, FILTER_SANITIZE_STRING);
+    $number = $_POST['number'];
+    $number = filter_var($number, FILTER_SANITIZE_STRING);
+    $pass = sha1($_POST['pass']);
+    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+    $cpass = sha1($_POST['cpass']);
+    $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+
+    $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? OR number = ?");
+    $select_user->execute([$email, $number]);
+    $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+    if ($select_user->rowCount() > 0) {
+        $message[] = 'email or number already exists!';
+    } else {
+        if ($pass != $cpass) {
+            $message[] = 'confirm password not matched!';
+        } else {
+            $insert_user = $conn->prepare("INSERT INTO `users`(name, email, number, password) VALUES(?,?,?,?)");
+            $insert_user->execute([$name, $email, $number, $cpass]);
+            $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ?");
+            $select_user->execute([$email, $pass]);
+            $row = $select_user->fetch(PDO::FETCH_ASSOC);
+            if ($select_user->rowCount() > 0) {
+                $_SESSION['user_id'] = $row['id'];
+                header('location:home.php');
+            }
+        }
+    }
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+
+    <!-- Swiper link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- custom css link -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+
+    <!-- header section starts -->
+    <?php include 'components/user_header.php'; ?>
+
+
+    <section class="form-container">
+
+        <form action="" method="post">
+            <h3>register now</h3>
+            <input type="text" name="name" required placeholder="enter your name" class="f-box" maxlength="50">
+            <input type="email" name="email" required placeholder="enter your email" class="f-box" maxlength="50"
+                oninput="this.value = this.value.replace(/\s/g, '')">
+            <input type="number" name="number" required placeholder="enter your number" class="f-box" min="0"
+                max="9999999999" maxlength="10">
+            <input type="password" name="pass" required placeholder="enter your password" class="f-box" maxlength="50"
+                oninput="this.value = this.value.replace(/\s/g, '')">
+            <input type="password" name="cpass" required placeholder="confirm your password" class="f-box" maxlength="50"
+                oninput="this.value = this.value.replace(/\s/g, '')">
+            <input type="submit" value="register now" name="submit" class="btn">
+            <p>already have an account? <a href="login.php">login now</a></p>
+        </form>
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer section -->
+
+    <?php include 'components/footer.php'; ?>
+
+    <!-- custom js file link -->
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
